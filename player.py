acb9a692987dcb0b8fb59e3533c350599bb1a9f0
Antonette <amgichohu@gmail.com>
2019-09-11 14:20:17 -0500

    very loose outline of player

---
+++ player.py
@@ -0,0 +1,17 @@
+
+
+class Player:
+
+    def __init__(self, name, board):
+        self.name=name
+        self.board = board
+        
+    def be_attacked(self):
+        #codey code code
+        return
+
+
+    def has_lost(self):
+        #codester McCode
+        print(self.name + " has lost")
+        return
