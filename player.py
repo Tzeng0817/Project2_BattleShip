3b34278326c67a3cb0f7dd0ed3428d06eaf91839
Grant Gollier <gragollier@gmail.com>
2019-09-16 10:47:40 -0500

    Merge 'ag/ship' into 'master'

--- player.py
+++ player.py
@@ -1,17 +1,25 @@
+##Player
+from board import Board
+
+class Player():
+    def __init__(self, name, num_of_ships):
+        self.name = name
+        self.num_of_ships = num_of_ships
+        self.lost = False
+        self.total_ship_blocks = self.get_ship_blocks(num_of_ships)
+        self.board = Board()
 
 
-class Player:
-
-    def __init__(self, name, board):
-        self.name=name
-        self.board = board
-        
     def be_attacked(self):
-        #codey code code
-        return
-
+        if self.total_ship_blocks > 0:
+            self.total_ship_blocks-=1
 
     def has_lost(self):
-        #codester McCode
-        print(self.name + " has lost")
-        return
+        self.lost = True
+        print(self.name + "has lost")
+
+    def get_ship_blocks(self, num_of_ships):
+        sum = 0
+        for i in range(1, num_of_ships+1):
+            sum+=i
+        return sum
