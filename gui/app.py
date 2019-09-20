c61c3523f244a59a7e2eae4e21288b5357b1f087
Grant Gollier <gragollier@gmail.com>
2019-09-20 16:48:55 -0500

    WIP: Add board GUI with arcade

--- gui/app.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QDialog, QApplication
-from gui.startMenu import Ui_start_menu
-
-class AppWindow(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_start_menu()
-        self.ui.setupUi(self)
-        self.show()
