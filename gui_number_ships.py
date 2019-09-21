8ebb63d5b5bb52babfaaeea3c5bf11e3262db037
Karenw1004 <karenw0103@gmail.com>
2019-09-21 12:10:36 -0500

    write 5 function when u click one of the 5 buttons

--- gui_number_ships.py
+++ gui_number_ships.py
@@ -13,21 +13,19 @@ SCREEN_TITLE = "Click on the number of ships"
 class NumberShips(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-
         arcade.set_background_color(arcade.color.AMAZON)
-        self.pause = False
 
     def setup(self):
         self.button_list = []
-        self.button_1 = NumberButton(110, 490, self.dummy_function, "1")
+        self.button_1 = NumberButton(110, 490, self.number_1, "1")
         self.button_list.append(self.button_1)
-        self.button_2 = NumberButton(250, 490, self.dummy_function, "2")
+        self.button_2 = NumberButton(250, 490, self.number_2, "2")
         self.button_list.append(self.button_2)
-        self.button_3 = NumberButton(390, 490, self.dummy_function, "3")
+        self.button_3 = NumberButton(390, 490, self.number_3, "3")
         self.button_list.append(self.button_3)
-        self.button_4 = NumberButton(530, 490, self.dummy_function, "4")
+        self.button_4 = NumberButton(530, 490, self.number_4, "4")
         self.button_list.append(self.button_4)
-        self.button_5 = NumberButton(670, 490, self.dummy_function, "5")
+        self.button_5 = NumberButton(670, 490, self.number_5, "5")
         self.button_list.append(self.button_5)
 
     def on_draw(self):
@@ -56,6 +54,30 @@ class NumberShips(arcade.Window):
     def dummy_function(self):
         print("hi")
 
+    @staticmethod
+    def number_1():
+        print("clicked on button 1")
+        return 1
+
+    @staticmethod
+    def number_2():
+        print("clicked on button 2")
+        return 2
+
+    @staticmethod
+    def number_3():
+        print("clicked on button 3")
+        return 3
+
+    @staticmethod
+    def number_4():
+        print("clicked on button 4")
+        return 4
+
+    @staticmethod
+    def number_5():
+        print("clicked on button 5")
+        return 5
 
 class TempView (arcade.View):
     def on_show(self):
