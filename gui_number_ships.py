8cf17d59a65dd3e581c3cd748545926625f251d6
Karenw1004 <karenw0103@gmail.com>
2019-09-21 15:31:51 -0500

    add documentation and delete all dummy function

--- gui_number_ships.py
+++ gui_number_ships.py
@@ -1,3 +1,6 @@
+"""
+Window with buttons and Text(How many ships)
+"""
 import arcade
 from button import Button
 from button import NumberButton
@@ -11,11 +14,21 @@ SCREEN_TITLE = "Click on the number of ships"
 
 
 class NumberShips(arcade.Window):
+    """
+    NumberShips class is the window for gui_number_ships
+    """
     def __init__(self, width, height, title):
+        """
+        Constructs a new NumberShips window.
+        :return: returns none.
+        """
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
+        """
+        Adding button to the button_list
+        """
         self.button_list = []
         self.button_1 = NumberButton(110, 490, self.number_1, "1")
         self.button_list.append(self.button_1)
@@ -29,67 +42,92 @@ class NumberShips(arcade.Window):
         self.button_list.append(self.button_5)
 
     def on_draw(self):
+        """
+        draw the text and the buttons in the list_button
+        """
         arcade.start_render()
         arcade.draw_text("How many Ships do you want?", 80, 530, arcade.color.BLACK, 40)
 
         for element in self.button_list:
             element.draw()
 
-        # arcade.finish_render()
-
     def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called whenever the mouse moves
+        :param: x (int): the x position of the mouse
+        :param: y (int): the y position of the mouse
+        """
         check_mouse_press_for_buttons(x, y, self.button_list)
-        # temp_view = TempView()
-        # self.window.show_view(temp_view)
 
     def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called whenever the released the mouse
+        :param: x (int): the x position of the mouse
+        :param: y (int): the y position of the mouse
+        """
         check_mouse_release_for_buttons(x, y, self.button_list)
 
     def pause_program(self):
+        """
+        pause the program
+        """
         self.pause = True
 
     def resume_program(self):
+        """
+        resume the program
+        """
         self.pause = False
-
-    def dummy_function(self):
-        print("hi")
 
     @staticmethod
     def number_1():
+        """
+        return 1 to the next window
+        :return: 1 (int)
+        """
         print("clicked on button 1")
         return 1
 
     @staticmethod
     def number_2():
+        """
+        return 2 to the next window
+        :return: 2 (int)
+        """
         print("clicked on button 2")
         return 2
 
     @staticmethod
     def number_3():
+        """
+        return 3 to the next window
+        :return: 3 (int)
+        """
         print("clicked on button 3")
         return 3
 
     @staticmethod
     def number_4():
+        """
+        return 4 to the next window
+        :return: 4 (int)
+        """
         print("clicked on button 4")
         return 4
 
     @staticmethod
     def number_5():
+        """
+        return 5 to the next window
+        :return: 5 (int)
+        """
         print("clicked on button 5")
         return 5
-
-class TempView (arcade.View):
-    def on_show(self):
-        arcade.set_background_color(arcade.color.BLACK)
 
 
 def main():
     window = NumberShips(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
-    # window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    # my_game = MyGame()
-    # window.show_view(my_game)
     arcade.run()
 
 
