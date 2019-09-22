195f522e03a9bd04b097abaaab71d7826403b0fb
Grant Gollier <gragollier@gmail.com>
2019-09-22 08:25:19 -0500

    Fix broken board class

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from game import Game
+from player import Player
+from ship import Direction
 import arcade
 
 if __name__ == "__main__":
-    GAME = Game(2)
+    player1 = Player(1)
+    player2 = Player(1)
+    player1.board.place_ships(1, (0, 0), Direction.RIGHT)
+    player2.board.place_ships(1, (1, 1), Direction.RIGHT)
+    GAME = Game(player1, player2)
     arcade.schedule(GAME.run, 0.25)
     arcade.run()
